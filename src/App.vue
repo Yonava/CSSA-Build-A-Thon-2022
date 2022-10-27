26b99775ba5cea1848594376561a54721fe47c9a
Yonava <yonavossandreae@gmail.com>
2022-10-27 19:43:24 -0400

    added faqs

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@
         <li>Get feedback from judges with industry experience</li>
         <li>Receive a cssa build-a-thon t-shirt</li>
         <li>Get a $10 amazon gift card for completed software, even if you don't win</li>
+        <li>Enjoy dinner provided by CSSA and SETA at the opening and closing ceremony</li>
       </ul>
       <h2 class="info-heading">The Prize Pool</h2>
       <ul>
@@ -45,6 +46,45 @@
           class="conf-link"
         >THAT Con</a>
         in exchange for the cash prize.
+      </p>
+      <h2 class="info-heading">What Am I Signing Up For?</h2>
+      <p>
+        The CSSA Software Build-A-Thon is a software building competition.
+        Teams of 2-3 will compete over Friday (18th) night, Saturday (19th), and Sunday (20th)
+        to build a piece of software based on a general theme that will be announced at
+        the opening ceremony on the 18th @ 6:30p SETA 118A/B.
+      </p>
+      <h2 class="info-heading">That's All Good, But How Does My Team Win?</h2>
+      <p>
+        Our judges will be evaluating every teams project
+        to determine the winner based on a set of criteria including
+        how the code is organized (software craftsmanship), how creative is your teams
+        idea and the approach your team uses to turn your idea into a reality.
+        Factors like design pattern implementation may be observed as well.
+      </p>
+      <h2 class="info-heading">FAQs</h2>
+      <p><b>Do I Need to Know How to Code?</b></p>
+      <p class="faq-answers">
+        Not necessarily, however, you should find a teammate who does know how to code.
+        Software development is mulit-facited. If you are a graphic designer
+        who has knowledge in Figma or Adobe product but are scared off by
+        the prospect of writing code, find someone who programs and build something
+        awesome together!
+      </p>
+      <p><b>What Language or Framework are Teams Required to Use?</b></p>
+      <p class="faq-answers">
+        We don't discriminate! If you are a game dev with a lot of Unity or
+        Unreal experience, build a game. If you are a front-end dev, build an
+        app or website. Back-end? make a console app or API. There will be
+        infinite ways to engage our theme. We are judging your approach to
+        building software, not the flashiness. We understand that building a
+        C++ app the renders ASCII graphics from scratch will be much harder to pull off than
+        using tools built into advanced game engines.
+      </p>
+      <p><b>Can I Still Sign-Up If I Don't Have a Team?</b></p>
+      <p class="faq-answers">
+        Sure. Just write "TBD" as your team name and we will match you
+        with others at the opening ceremony!
       </p>
     </div>
   </div>
@@ -124,6 +164,10 @@ div.info {
 }
 
 h2.info-heading {
+  margin: 0%;
+}
+
+p.faq-answers {
   margin: 0%;
 }
 
